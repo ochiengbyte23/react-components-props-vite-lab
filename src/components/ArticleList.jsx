6f26aa1ab53285ca3f0d1ed3a577b4c66c2e0ce4
Bildad Ochieng Masaga <bijasoto@gmail.com>
2026-01-16 16:10:30 +0300

    created Article prop child connection

---
+++ src/components/ArticleList.jsx
@@ -0,0 +1,17 @@
+import Article from "./Article";
+
+export default function ArticleList({ posts }) {
+    return (
+        <main>
+            {posts.map((post) => (
+                <Article
+                    key={post.id}
+                    title={post.title}
+                    date={post.date}
+                    preview={post.preview}
+                    minutes={post.minutes}
+                />
+            ))}
+        </main>
+    );
+};
